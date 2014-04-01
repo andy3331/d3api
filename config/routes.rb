9a78aef33882ca0d30e216a1e86a5393e4846e4c
Andy Connington <aconnington@gmail.com>
2014-04-01 09:44:05 -0400

    first pages/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 D3api::Application.routes.draw do
+  
+root "pages#home"
+
+  # get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
